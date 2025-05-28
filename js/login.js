6e186d6a7be655817027037b79f67431f08a4db8
juan esteban gaviria <cocoiral@hotmail.com>
2025-05-27 19:44:38 -0500

    crear clase persona. js

--- js/login.js
+++ js/login.js
@@ -1,10 +1,26 @@
-
+   import { persona } from "/js/persona.js"; 
 const contenedor = document.getElementById('contenedor');
 const botonRegistrarse = document.getElementById('registrarse');
 const botonIniciarSesion = document.getElementById('iniciarSesion');
 const botonRegresar = document.getElementById('regresar');
 
+
+
 botonRegistrarse.addEventListener('click', () => {
+ 
+      const nuevoUsuario = new persona({
+        
+        nombres: document.getElementById('nombres').value,
+        apellidos: document.getElementById('apellidos').value,
+        celular: document.getElementById('celular').value,
+        documento: document.getElementById('documento').value,
+        usuario: document.getElementById('usuario').value,
+        contrasena: document.getElementById('contrasena').value,
+        email: document.getElementById('email').value,  
+    
+    });
+     console.log(nuevoUsuario); 
+
     contenedor.classList.add("activo");
 });
 
---
+++ js/persona.js
@@ -0,0 +1,55 @@
+ export class persona {
+  constructor({nombres, apellidos, celular, documento, usuario, contrasena,email}){
+    this._nombres = nombres;
+    this._apellidos = apellidos;    
+    this._celular = celular;
+    this._documento = documento;
+    this._usuario = usuario;  
+    this._contrasena = contrasena;
+    this._email = email;  
+
+  }
+  set setnombres(nombres) {
+    this._nombres = nombres;
+  }
+  get getnombres() {
+    return this._nombres;
+  }
+  set setapellidos(apellidos) {
+    this._apellidos = apellidos;
+  }
+  get getapellidos() {
+    return this._apellidos;
+  } 
+  set setcelular(celular) {
+    this._celular = celular;
+  } 
+  get getcelular() {
+    return this._celular;
+  }
+  set setdocumento(documento) {
+    this._documento = documento;
+  }
+  get getdocumento() {
+    return this._documento;
+  }
+  set setusuario(usuario) {
+    this._usuario = usuario;
+  }
+  get getusuario() {
+    return this._usuario;
+  }
+  set setcontrasena(contrasena) {
+    this._contrasena = contrasena;
+  }
+  get getcontrasena() {
+    return this._contrasena;
+  }
+  set setemail(email) {
+    this._email = email;
+  } 
+  get getemail() {
+    return this._email;
+  } 
+
+}
